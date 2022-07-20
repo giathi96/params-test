e5fed292a55f9310024845b6571b6feacd1a3b01
rlgthi <giathirahlan@gmail.com>
2022-07-20 17:10:36 +0700

    update

--- params.groovy
+++ params.groovy
@@ -1,71 +1,116 @@
-features =[
-     ["channel_name": "Chat", "traffic_rate": "" ,"placeholder":"Traffic rate for Chat", "value":"chat"],
-    ["channel_name": "Email", "traffic_rate": "" ,"placeholder":"Traffic rate for Email","value":"email"],
-    ["channel_name": "Voice", "traffic_rate": "" ,"placeholder":"Traffic rate for Voice","value":"voice"],
-    ["channel_name": "Messaging", "traffic_rate":"" ,"placeholder":"Traffic rate for Messaging","value":"messaging"],
-]
-   
-
-html_to_be_rendered = """
+html ="""
 <style>
+html,
+    body,
+    div,
+    span {
+        margin: 0;
+        padding: 0;
+        border: 0;
+        font-size: 100%;
+        vertical-align: baseline;
+    }
+
+    article,
+    aside,
+    details,
+    figcaption,
+    figure,
+    footer,
+    header,
+    hgroup,
+    menu,
+    nav,
+    section {
+        display: block;
+    }
+
+    body {
+        line-height: 1.5;
+    }
+
+    ol,
+    ul {
+        list-style: none;
+    }
+
+    blockquote,
+    q {
+        quotes: none;
+    }
+
+    blockquote:before,
+    blockquote:after,
+    q:before,
+    q:after {
+        content: '';
+        content: none;
+    }
+
     table {
         border-collapse: collapse;
         border-spacing: 0;
-        width: 500px
-    }
-    .traffic-text {
-        width: 390px;
-        margin: auto;
+        width: 500px;
     }
 
-    .checkbox {
-        width: 100px;
-        border-radius: 4px;
-        margin: auto;
-    }
-    .row {
-        width: 100%;
-        margin-top: 10px;
-    }
-    .text {
-        padding: 5px;
-        border: 0px;
-        width: 100%;
-        display: inline-block;
+    .task-select {
+        padding: 5px 5px;
+        width: 500px;
         border: 1px solid #ccc;
         border-radius: 4px;
-        box-sizing: border-box;
+        resize: vertical;
     }
-    .features_des{
+
+    .task_des {
         padding: 5px 5px;
         display: inline-block;
         font-style: italic;
         font-weight: lighter;
         font-size: 80%;
     }
+
+    .features_des {
+        padding: 5px 5px;
+        display: inline-block;
+        font-style: italic;
+        font-weight: lighter;
+        font-size: 80%;
+    }
+
+    .row {
+        width: 100%;
+        margin-top: 10px;
+    }
+    .style{
+        font: inherit;
+    }
 </style>
-<table>
-<tr class="row">
-        <span class="features_des">Select features and traffic rate</span>
-</tr>
+<div>
+   <table>
+            <tr>
+                <td>
+                    <span class="task_des">Choose the task you want to do</span>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <select name='value' class='task-select'>
+                        <option value="skip" >Skip stage</option>
+                        <option value="all" >All</option>
+                          <option value="create" >Create Tenant</option>
+                          <option value="deploy">Deployment</option>
+                     </select>
+                </td>
+            </tr>
+            <tr >
+            <td><br><span class="features_des">Select features for tenant</span><br></td>
+            </tr>
+            <tr>
+            <td><input name='value' alt='voice' json='voice' type='checkbox' class='' value='voice'><label title='voice'
+                class='' style="padding-left: 30px;">Messaging</label></td>
+        </tr>
+        </table>
+</div>
 """
-features.each { feature ->
-  html_to_be_rendered = """
-    ${html_to_be_rendered}
-    <tr class="row">
-    <td class="checkbox">
-    <input name='value' alt='${feature.value}' json='${feature.value}' type='checkbox' class='' value='${feature.value}'>
-    <label title='${feature.channel_name}' class=''>${feature.channel_name}</label>
-    </td>
-    <td class='traffic-text'>
-    <input type='text' class='text' name='value' value='' placeholder='${feature.placeholder}'> </br>
-    </td>
-    </tr>
-"""
-}
-
-
-html_to_be_rendered = "${html_to_be_rendered}</table>"
-
-println(html_to_be_rendered)
-return html_to_be_rendered
+println(html)
+return html
