38142327f655107c5f0b8edb6e5bda9726e2f102
rlgthi <giathirahlan@gmail.com>
2022-08-06 14:49:05 +0700

    update

--- params.groovy
+++ params.groovy
@@ -1,15 +1,31 @@
+branchs = [
+    ["branch_name": "Master", "value": "master" ],
+    ["branch_name": "Dev", "value": "dev" ],
+]
+
 html ="""
-<style>
-</style>
 <div>
-<table>
-    <tr>
-        <td><span class = "params_des">If value is 0 or blank, will skip traffic test for this feature</span></td>
-    </tr>
-    <tr>
-        <td><input name='value' value='' class='text_input' type='text' placeholder='0'></td>
-    </tr>
-</table>
+   <table>
+            <tr>
+                <td>
+                    <span class="params_des">Select the lab you want to run the script.</span>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <select name='value' class='params_select'>
+                        
+"""
+branchs.each { branch ->
+html = """${html}
+<option value='${branch.value}'>${branch.branch_name}</option>
+"""
+}
+html = """${html}
+                   </select>
+                </td>
+            </tr>
+        </table>
 </div>
 """
 return html
